f4104d0155d05b16a49c4aa59b1ac34f644f59dd
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-08-05 16:17:06 -0400

    Removed core app

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "drf_spectacular",
-    "core",
     "selection",
     "subject",
     "user",
@@ -175,7 +174,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "core.User"
+AUTH_USER_MODEL = "user.User"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
