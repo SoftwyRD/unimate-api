461e6fd269c0bb61eb26c65e9539f006a21c227a
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-08-07 08:42:52 -0400

    Refactored user app

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,46 +1,35 @@
-"""User serializers."""
-
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework_simplejwt.serializers import (
-    TokenObtainPairSerializer,
-    RefreshToken,
-)
+from rest_framework.serializers import ModelSerializer
 
 
-class UserSerializer(serializers.ModelSerializer):
-    """Serializer for User"""
-
+class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = [
             "id",
             "first_name",
-            "middle_name",
             "last_name",
             "username",
-            "password",
             "email",
+            "password",
         ]
         extra_kwargs = {
             "id": {
                 "read_only": True,
             },
+            "username": {
+                "min_length": 2,
+            },
+            "email": {
+                "min_length": 2,
+            },
             "password": {
                 "write_only": True,
-                "min_length": 8,
+                "min_length": 9,
             },
         }
 
-    def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
-
-        return get_user_model().objects.create(**validated_data)
-
     def update(self, instance, validated_data):
-        """Update a user, setting the password correctly and return it"""
-
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
 
@@ -49,57 +38,3 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
-
-
-class PairTokenSerializer(TokenObtainPairSerializer):
-    """Serializer for TokenObtainPair"""
-
-    def validate(self, attrs):
-        """Validate the data before saving it"""
-        try:
-            data = super().validate(attrs)
-            response = {
-                "status": "success",
-                "data": {
-                    "tokens": data,
-                },
-            }
-
-            return response
-        except AuthenticationFailed as ex:
-            response = {
-                "status": "fail",
-                "data": {
-                    "title": "No active account",
-                    "message": str(ex),
-                },
-            }
-
-            raise AuthenticationFailed(response)
-
-
-class RefreshTokenSerializer(RefreshToken):
-    """Serializer for RefreshToken"""
-
-    def validate(self, attrs):
-        """Validate the data before saving it"""
-        try:
-            data = super().validate(attrs)
-            response = {
-                "status": "success",
-                "data": {
-                    "token": data,
-                },
-            }
-
-            return response
-        except AuthenticationFailed as ex:
-            response = {
-                "status": "fail",
-                "data": {
-                    "title": ex.args[1],
-                    "message": ex.args[0],
-                },
-            }
-
-            raise AuthenticationFailed(response)
--- app/user/views.py
+++ app/user/views.py
@@ -1,275 +1,164 @@
-"""User views"""
-
-from django.contrib.auth import get_user_model
 from django.urls import reverse
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from user import serializers
-from user.permissions import PublicPostRequest
-from drf_spectacular.utils import extend_schema
 
-SCHEMA_NAME = "user"
+from user.serializers import UserSerializer
+
+SCHEMA_NAME = "users"
 
 
-def user_location_url(user_id):
-    """Get reverse url for user details"""
-
-    return reverse("user:details", args=[user_id])
-
-
-@extend_schema(
-    tags=[SCHEMA_NAME],
-)
-class PairTokenView(TokenObtainPairView):
-    """View for getting acces and refresh token for user"""
-
-    serializer_class = serializers.PairTokenSerializer
-
-
-@extend_schema(
-    tags=[SCHEMA_NAME],
-)
-class RefreshTokenView(TokenRefreshView):
-    """View for refreshing access token for user"""
-
-    serializer_class = serializers.RefreshTokenSerializer
+def user_profile_url(request):
+    user_profile_url = reverse("user:profile")
+    absolute_url = request.build_absolute_uri(user_profile_url)
+    return absolute_url
 
 
 @extend_schema(tags=[SCHEMA_NAME])
-class UserListView(APIView):
-    """View for list users in api"""
-
-    permission_classes = [PublicPostRequest]
-    serializer_class = serializers.UserSerializer
+class SignUpView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
 
     @extend_schema(
-        operation_id="Retrieve users list",
-        description="Retrieves all the users.",
-    )
-    def get(self, request, format=None):
-        """Get all users"""
-
-        try:
-            users = get_user_model().objects.all()
-            serializer = self.serializer_class(users, many=True)
-
-            response = {
-                "status": "success",
-                "data": {
-                    "count": users.count(),
-                    "users": serializer.data,
-                },
-            }
-            return Response(response, status.HTTP_200_OK)
-        except Exception as ex:
-            response = {
-                "status": "error",
-                "message": ex,
-            }
-            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    @extend_schema(
-        operation_id="Create user",
-        description="Creates a new user.",
+        operation_id="Sign up",
+        description="Registers a new user.",
+        request=UserSerializer,
+        responses={
+            201: UserSerializer,
+        },
     )
     def post(self, request, format=None):
-        """Create new user"""
-
         try:
             data = request.data
-            serializer = self.serializer_class(data=data, many=False)
+            serializer = UserSerializer(data=data)
 
-            if serializer.is_valid():
-                serializer.save()
-                user = serializer.data
-
-                headers = {
-                    "Location": user_location_url(user["id"]),
-                }
+            if not serializer.is_valid():
                 response = {
-                    "status": "success",
-                    "data": {
-                        "user": user,
-                    },
+                    "title": "Could not sign up",
+                    "details": serializer.errors,
                 }
-                return Response(
-                    response, status.HTTP_201_CREATED, headers=headers
-                )
+                return Response(response, status.HTTP_400_BAD_REQUEST)
+            serializer.save()
 
-            response = {
-                "status": "fail",
-                "data": {
-                    "title": "Could not register the user",
-                    "message": serializer.errors,
-                },
+            headers = {
+                "Location": user_profile_url(request),
             }
-            return Response(response, status.HTTP_400_BAD_REQUEST)
+            response = serializer.data
+            return Response(response, status.HTTP_201_CREATED, headers=headers)
 
-        except Exception as ex:
+        except Exception:
             response = {
-                "status": "error",
-                "message": ex,
+                "title": "Error",
+                "message": "There was an error trying to sign up.",
             }
             return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @extend_schema(tags=[SCHEMA_NAME])
-class UserDetailsView(APIView):
-    """View for user details in api"""
-
-    permission_classes = [IsAdminUser]
-    serializer_class = serializers.UserSerializer
-
-    @extend_schema(
-        operation_id="Retreave user details",
-        description="Retrieves the specified user details.",
-    )
-    def get(self, request, id, format=None):
-        """Get user details"""
-
-        try:
-            user = get_user_model().objects.get(id=id)
-            serializer = self.serializer_class(user, many=False)
-
-            response = {
-                "status": "success",
-                "data": {
-                    "user": serializer.data,
-                },
-            }
-            return Response(response, status.HTTP_200_OK)
-        except Exception as ex:
-            response = {
-                "status": "error",
-                "message": ex,
-            }
-            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    @extend_schema(
-        operation_id="Partial update user details",
-        description="Partially updates the specified user details.",
-    )
-    def patch(self, request, id, format=None):
-        """Update user details"""
-
-        try:
-            data = request.data
-            user = get_user_model().objects.get(id=id)
-            serializer = self.serializer_class(user, data=data, many=False)
-
-            if serializer.is_valid():
-                serializer.save()
-
-                response = {
-                    "status": "success",
-                    "data": {
-                        "user": serializer.data,
-                    },
-                }
-
-                return Response(response, status.HTTP_200_OK)
-
-            response = {
-                "status": "fail",
-                "data": {
-                    "title": "Could not update the user",
-                    "message": serializer.errors,
-                },
-            }
-            return Response(response, status.HTTP_400_BAD_REQUEST)
-        except Exception as ex:
-            response = {
-                "status": "error",
-                "message": ex,
-            }
-            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    @extend_schema(
-        operation_id="Delete user",
-        description="Deletes the specified user.",
-    )
-    def delete(self, request, id, format=None):
-        """Delete user"""
-
-        try:
-            user = get_user_model().objects.get(id=id)
-            user.delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Exception as ex:
-            response = {
-                "status": "error",
-                "message": ex,
-            }
-            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-@extend_schema(tags=[SCHEMA_NAME])
-class MeView(APIView):
-    """View for logged user details in api"""
-
+class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = serializers.UserSerializer
 
     @extend_schema(
-        operation_id="Retreave profile",
+        operation_id="Retrieve profile",
         description="Retrieves the requesting user profile.",
+        request=UserSerializer,
+        responses={
+            200: UserSerializer,
+        },
     )
     def get(self, request, format=None):
-        """Get logged user details"""
-        user = request.user
-        serializer = self.serializer_class(user, many=False)
-        response = {
-            "status": "success",
-            "data": {
-                "profile": serializer.data,
-            },
-        }
-        return Response(response, status.HTTP_200_OK)
+        try:
+            user = request.user
+            serializer = UserSerializer(user)
+            response = serializer.data
+            return Response(response, status.HTTP_200_OK)
+        except Exception:
+            response = {
+                "title": "Error",
+                "message": "There was an error trying to retrieve your profile.",
+            }
+            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @extend_schema(
+        operation_id="Update user profile",
+        description="Updates the requesting user profile.",
+        request=UserSerializer,
+        responses={
+            200: UserSerializer,
+        },
+    )
+    def put(self, request, format=None):
+        try:
+            user = request.user
+            data = request.data
+            serializer = UserSerializer(user, data=data)
+
+            if not serializer.is_valid():
+                response = {
+                    "title": "Could not update your profile",
+                    "details": serializer.errors,
+                }
+                return Response(response, status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+
+            response = serializer.data
+            return Response(response, status.HTTP_200_OK)
+        except Exception:
+            response = {
+                "title": "Error",
+                "message": "There was an error trying to update your profile.",
+            }
+            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @extend_schema(
         operation_id="Partial update user profile",
         description="Partially updates the requesting user profile.",
+        request=UserSerializer,
+        responses={
+            200: UserSerializer,
+        },
     )
     def patch(self, request, format=None):
-        """Update logged user details"""
-
         try:
-            data = request.data
             user = request.user
-            serializer = self.serializer_class(
-                user, data=data, many=False, partial=True
-            )
+            data = request.data
+            serializer = UserSerializer(user, data=data, partial=True)
 
-            if serializer.is_valid():
-                serializer.save()
-
+            if not serializer.is_valid():
                 response = {
-                    "status": "success",
-                    "data": {
-                        "profile": serializer.data,
-                    },
+                    "title": "Could not update your profile",
+                    "details": serializer.errors,
                 }
+                return Response(response, status.HTTP_400_BAD_REQUEST)
+            serializer.save()
 
-                return Response(response, status.HTTP_200_OK)
-
+            response = serializer.data
+            return Response(response, status.HTTP_200_OK)
+        except Exception:
             response = {
-                "status": "fail",
-                "data": {
-                    "title": "Could not update the user",
-                    "message": serializer.errors,
-                },
+                "title": "Error",
+                "message": "There was an error trying to update your profile.",
             }
-            return Response(response, status.HTTP_400_BAD_REQUEST)
-        except Exception as ex:
+            return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @extend_schema(
+        operation_id="Delete user profile",
+        description="Deletes the requesting user profile.",
+        responses={
+            204: None,
+        },
+    )
+    def delete(self, request, format=None):
+        try:
+            user = request.user
+            user.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception:
             response = {
-                "status": "error",
-                "message": ex,
+                "title": "Error",
+                "message": "There was an error trying to delete your profile.",
             }
             return Response(response, status.HTTP_500_INTERNAL_SERVER_ERROR)
