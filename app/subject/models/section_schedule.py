62707edb10d597e75cc5f33212364617b8d0bb77
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-08-14 12:29:11 -0400

    Replaced migrations, updated fixtures and models

--- app/subject/models/section_schedule.py
+++ app/subject/models/section_schedule.py
@@ -1,24 +1,53 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-from .weekday import Weekday
 from .subject_section import SubjectSection
+from .weekday import Weekday
 
 
 class SectionSchedule(models.Model):
-    id = models.AutoField(primary_key=True, unique=True, editable=False)
-    section = models.ForeignKey(
-        SubjectSection,
-        on_delete=models.CASCADE,
-        related_name="subject_schedule",
+    id = models.AutoField(
+        verbose_name=_("id"),
+        help_text=_("Section's schedule id."),
+        primary_key=True,
+        unique=True,
+        editable=False,
     )
-    weekday = models.ForeignKey(Weekday, on_delete=models.SET_NULL, null=True)
+    section = models.ForeignKey(
+        verbose_name=_("selection"),
+        help_text=_("Section's id."),
+        to=SubjectSection,
+        on_delete=models.CASCADE,
+        related_name="schedule",
+        related_query_name="schedule",
+    )
+    weekday = models.ForeignKey(
+        verbose_name=_("weekday"),
+        help_text=_("Weekday id."),
+        to=Weekday,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="weekday",
+        related_query_name="weekday",
+    )
     start_time = models.IntegerField(
-        default=7, validators=[MinValueValidator(7), MaxValueValidator(20)]
+        verbose_name=_("start time"),
+        help_text=_("Section's start time."),
+        default=6,
+        validators=[MinValueValidator(6), MaxValueValidator(20)],
     )
     end_time = models.IntegerField(
-        default=9, validators=[MinValueValidator(9), MaxValueValidator(22)]
+        verbose_name=_("end time"),
+        help_text=_("Section's end time."),
+        default=8,
+        validators=[MinValueValidator(8), MaxValueValidator(22)],
     )
+
+    class Meta:
+        verbose_name = _("section schedule")
+        verbose_name_plural = _("section schedules")
+        db_table = "section_schedule"
 
     def __str__(self):
         return str(self.id)
