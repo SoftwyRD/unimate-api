571f2961222e973b782ca6327466e6976c503a72
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2023-09-10 21:05:34 -0400

    Fixed admin pages for selection and subject section

--- subject/admin/subject_section_admin.py
+++ subject/admin/subject_section_admin.py
@@ -1,25 +1,18 @@
-from django.contrib.admin import ModelAdmin, TabularInline, register
+from django.contrib.admin import ModelAdmin, register
 
-from ..models import SectionSchedule, SubjectSection
-
-
-class SectionScheduleInline(TabularInline):
-    model = SectionSchedule
-    extra = 0
-    verbose_name = "schedule"
-    verbose_name_plural = "schedules"
+from ..models import SubjectSection
 
 
 @register(SubjectSection)
 class SubjectSectionAdmin(ModelAdmin):
-    ordering = ("selection",)
-    list_display = ("selection", "subject", "section")
-    list_filter = ("taken",)
+    ordering = ("subject",)
+    list_display = ("subject", "section")
+    list_filter = ("selected_on__is_active",)
     search_fields = (
-        "selection__name",
+        "selected_on__selection__name",
         "subject__name",
         "subject__code",
-        "selection__user__username",
+        "selected_on__selection__user__username",
     )
     show_full_result_count = True
     list_per_page = 25
@@ -38,8 +31,6 @@ class SubjectSectionAdmin(ModelAdmin):
             },
         ),
     )
-
-    inlines = (SectionScheduleInline,)
 
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = self.readonly_fields
